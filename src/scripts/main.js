5efda99d5d5124e5a86234b2a383c551d8f481cb
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-30 21:28:26 +0300

    solution fetch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
 'use strict';
 
 // write your code here
+
+const phonesUrl = 'https://mate-academy.github.io/phone-catalogue-static/api';
+
+const options = {
+  method: 'GET',
+};
+
+const listOfPhones = document.createElement('ul');
+
+const getPhones = () => {
+  return new Promise((resolve, reject) => {
+    fetch(`${phonesUrl}/phones.json`, options)
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        resolve(result);
+      });
+
+    setTimeout(() => {
+      reject(new Error());
+    }, 5000);
+  });
+};
+
+const createListOfPhones = (data) => {
+  listOfPhones.insertAdjacentHTML('afterbegin', `
+     ${data.map(phone => phone.name).map(item => `<li>${item}</li>`).join(' ')}
+   `);
+
+  document.body.append(listOfPhones);
+
+  return data;
+};
+
+const getPhonesDetails = (list) => {
+  const fetchedList = list.map(phone => phone.id)
+    .map(id => fetch(`${phonesUrl}/phones/${id}.json`));
+
+  return Promise.all(fetchedList);
+};
+
+getPhones()
+  .then(createListOfPhones)
+  .then(getPhonesDetails)
+  .catch(error => {
+    alert(error);
+  });
